0901ae089f0f7a213bf26843817f6e976ed144dc
JoeBuydemDips <ksarkodie111@icloud.com>
2022-03-04 18:17:44 -0500

    Set up basic route structuring.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import pages
-import Home from './pages/Home'
-import About from './pages/About'
-import SingleCocktail from './pages/SingleCocktail'
-import Error from './pages/Error'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
 // import components
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
-  )
+    <Router>
+      <Navbar />
+      <Switch>
+        {/* Home page*/}
+        <Route exact path="/">
+          <Home />
+        </Route>
+        {/* About page page*/}
+        <Route path="/about">
+          <About />
+        </Route>
+        {/* cocktail page*/}
+        <Route path="/cocktail/:id">
+          <SingleCocktail />
+        </Route>
+        {/* Error page*/}
+        <Route path="*">
+          <Error />
+        </Route>
+        {/* end switch block*/}
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
